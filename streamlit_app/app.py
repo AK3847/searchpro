afe32241540a5545fd319a0c076063cc411a9b55
AK3847 <abdulkadir.co21d2@scet.ac.in>
2024-11-13 23:18:52 +0530

    frontend base done

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,7 +2,30 @@ import streamlit as st
 import requests
 import json
 
-st.title("searchpro - LLM")
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+
+    html, body, [class*="css"] {
+        font-family: 'Poppins', sans-serif;
+    }
+    
+    .stMarkdown {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    h1, h2, h3 {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 600;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.title("SearchPro")
+st.write("_Websearch powered by LLM_")
 
 user_query = st.text_input("Enter your query:")
 
@@ -12,10 +35,30 @@ if st.button("Search"):
         headers = {"Content-Type": "application/json"}
         data = {"query": f"{user_query}"}
 
-        response = requests.post(backend_api, headers=headers, data=json.dumps(data))
+        with st.spinner("_Searching the web...._"):
+            response = requests.post(
+                backend_api, headers=headers, data=json.dumps(data)
+            )
         if response.status_code == 200:
-            result = response.json().get("response", "No response")
-            st.write(result)
+            result = response.json()
+            result = result.get("response", {})
+            result = result.strip("```").lstrip("json")
+            result = json.loads(result)
+
+            sources = result.get("sources", [])
+            answer = result.get("response", {})
+
+            tabs = st.tabs(["Answer", "Sources"])
+
+            with tabs[0]:
+                st.markdown("### Answer")
+                st.markdown(answer["text"])
+
+            with tabs[1]:
+                st.markdown("### Sources")
+                for source in sources:
+                    st.markdown(f"-[{source['description']}]({source['link']})")
+
         else:
             st.write("Error:", response.status_code)
     else:
